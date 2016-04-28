e4779041a9dbe97880467983e6258a45e27a096d
Laura Sammons <lsammons@gmail.com>
2016-04-27 19:08:05 -0700

    write code to create object for portfolio project templates

--- scripts/project.js
+++ scripts/project.js
@@ -1,7 +1,7 @@
 var projects = [];
 
 function Project (opts) {
-  // DONE: Use the js object passed in to complete this contructor function:
+  // Use the js object passed in - opts - to complete this contructor function:
   // Save ALL the properties of `opts` into `this`.
   this.title = opts.title;
   this.category = opts.category;
@@ -12,43 +12,38 @@ function Project (opts) {
 }
 
 Project.prototype.toHtml = function() {
-  var $newProject = $('project.template').clone();
-
+  var $newProject = $('article.template').clone();
   $newProject.attr('data-category', this.category);
-
-  // TODO: Use jQuery to fill in the template with properties
-  // from this particular Project instance. We need to fill in:
-  // the author name and url, the Project title and body, and the
+  //Use jQuery to fill in the template with properties
+  // from this particular Article instance. We need to fill in:
+  // the author name and url, the article title and body, and the
   // publication date.
-
-  //$('h1').html(this.title);
-
   $newProject.find('h1').html(this.title);
-
+  $newProject.find('address a').html(this.author);
+  $newProject.find('section.article-body').html(this.body);
+  $newProject.find('address a').attr('href', this.authorUrl);
   // Include the publication date as a 'title' attribute to show on hover:
   $newProject.find('time[pubdate]').attr('title', this.publishedOn);
-
   // Display the date as a relative number of "days ago":
   $newProject.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-
   $newProject.append('<hr>');
-
-  // TODO: This cloned Project is no longer a template, so we should remove that class...
-
+  // DONE: This cloned article is no longer a template, so we should remove that class...
   // Remove Project.template so that this will show on page:
-  $('project').removeClass('template');
-
+  $('#theTemplate').hide();
+  // for some reason the last project through doesn't get class template removed?
+  // DEBUG THIS
+  $('article').removeClass('template');
   return $newProject;
 };
-
+// Find how long ago, from today, this project was created.
 rawData.sort(function(a,b) {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
-
+// Push each new project into an array
 rawData.forEach(function(ele) {
   projects.push(new Project(ele));
 });
-
+// Append each project to the #projects element in index.html
 projects.forEach(function(a){
   $('#projects').append(a.toHtml());
 });
